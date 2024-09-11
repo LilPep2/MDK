dfa39e32f19718bd41805b14f839f8375c185d18
Alex <123286654+LilPep2@users.noreply.github.com>
2024-09-11 12:34:49 +0400

    Add files via upload

---
+++ 3proga/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3proga
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                pictureBox2.Hide();
+                pictureBox1.Show();
+
+
+            }
+            else
+            {
+                pictureBox1.Hide();
+                pictureBox2.Show();
+            }
+        }
+
+
+    }
+}
